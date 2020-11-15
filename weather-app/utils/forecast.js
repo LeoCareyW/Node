14e193dd928b340628d17cbe592522fdc9e0b714
Leo Carey-Williams <leocareywilliams@gmail.com>
2020-11-15 23:03:00 +0000

    weather app changes made

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,7 +1,9 @@
+
+
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key=60aa268da685d9f91362657ffd6f5ace' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
